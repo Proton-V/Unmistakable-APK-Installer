47dce0c2144fc93bf7470dd20c4955c1f787e942
Proton-V <65833201+Proton-V@users.noreply.github.com>
2022-07-20 04:13:47 +0400

    Add base logic for APK Installer

---
+++ UnmistakableAPKInstaller/UnmistakableAPKInstaller.Helpers/Models/GoogleDrive/FileData.cs
@@ -0,0 +1,10 @@
+﻿namespace UnmistakableAPKInstaller.Helpers.Models.GoogleDrive
+{
+    public class FileData
+    {
+        public string kind { get; set; }
+        public string id { get; set; }
+        public string name { get; set; }
+        public string mimeType { get; set; }
+    }
+}
